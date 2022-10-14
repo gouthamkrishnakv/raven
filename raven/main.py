4e23d83c2e948d70e9ec27830603fea8d3972685
Goutham Krishna K V <gauthamkrishna9991@live.com>
2022-10-14 21:42:33 +0530

    feat(async): Initial App

--- raven/main.py
+++ raven/main.py
@@ -1,103 +1,69 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from queue import Queue
-from signal import SIGINT, sigwait
-from threading import Event, Thread
-from typing import Any
-from evdev import InputEvent, InputDevice
+from asyncio import Event, Queue
+import asyncio
 import logging
+from signal import SIGINT, sigwait
+from threading import Thread
+from evdev import InputDevice
 
 
-logging.basicConfig(level=logging.DEBUG)
+class AppThread(Thread):
+    logger = logging.getLevelName("AppThread")
 
-
-class InputThread(Thread):
-    """
-    Input Thread Class
-
-    Takes in input, and puts it into a queue.
-    """
-
-    logger = logging.getLogger("InputThread")
     stop_ev: Event
-    iqueue: Queue[InputEvent]
+    iqueue: Queue
     input_device: InputDevice
-    # 120Hz is taken from the fact that most touch sample rate is 2x touchpad resolution
-    # Expecting 120Hz * 8 as there can be at most 8 individual requests sent
+
     delay: float = 1 / (120 * 8)
 
-    def __init__(self, input_device: str):
+    def __init__(self, dev: str = "/dev/input/event8"):
         Thread.__init__(self)
         self.stop_ev = Event()
         self.iqueue = Queue()
-        self.input_device = InputDevice(input_device)
+        self.input_device = InputDevice(dev)
 
     def run(self):
+        asyncio.run(self.mainloop())
+
+    async def read_inp(self):
+        print("RI: START")
         self.input_device.grab()
-        self.logger.info("> POINTER GRABBED <")
-        while not self.stop_ev.wait(self.delay):
+        while not self.stop_ev.is_set():
             if (input_ev := self.input_device.read_one()) is not None:
-                self.iqueue.put(input_ev)
-        self.input_device.ungrab()
-        self.input_device.close()
-        self.logger.info("> POINTER UNGRABBED <")
+                await self.iqueue.put(input_ev)
+            await asyncio.sleep(self.delay)
+        print("RI: END")
 
-
-class ConsoleOutput(Thread):
-    """
-    Output Console Thread Class
-
-    Accepts input, then prints back the output.
-    """
-
-    logger = logging.getLogger("ConsoleOutput")
-    stop_ev: Event
-    iqueue: Queue[Any]
-
-    def __init__(self, queue: Queue[Any] = Queue()):
-        Thread.__init__(self)
-        self.stop_ev = Event()
-        self.iqueue = queue
-
-    def run(self):
-        self.logger.info("STARTED")
-        # Delay of 1 ms
-        while not self.stop_ev.wait(0.000_1):
-            if self.iqueue.qsize() > 0:
-                self.logger.info("IREP: %s", str(self.iqueue.get()))
-        self.logger.info("STOPPING")
+    async def write_console(self):
+        print("CW: START")
+        while not self.stop_ev.is_set():
+            if not self.iqueue.empty():
+                print(await self.iqueue.get())
+            await asyncio.sleep(0.000_1)
+        print("CW: STOPPING")
         while not self.iqueue.empty():
-            self.logger.info("IREP: %s", str(self.iqueue.get()))
-        self.logger.info("STOPPED")
+            print(await self.iqueue.get())
+        print("CW: END")
+
+    async def mainloop(self):
+        asyncio.create_task(self.read_inp())
+        asyncio.create_task(self.write_console())
+        while not self.stop_ev.is_set():
+            await asyncio.sleep(0.1)
+        asyncio.get_event_loop().stop()
 
 
 def main():
-    """
-    Main function
-    """
-    logger = logging.getLogger("main")
-    # Create pointer input and console output threads
-    logger.info("Creating input & output threads")
-    it = InputThread("/dev/input/event4")
-    ct = ConsoleOutput(it.iqueue)
-    # Set the threads to run
-    logger.info("Set the threads to run")
-    # Start the threads
-    logger.info("Start the threads")
-    it.start()
-    ct.start()
-    # Wait for interrupt signal
-    logger.info(">> To stop the application, click Ctrl+C")
+    apt = AppThread()
+    print("STARTING THREAD")
+    apt.start()
+
+    print("To stop app, click Ctrl+C")
     sigwait([SIGINT])
-    # Stopping threads, first output, then input
-    logger.info("Stopping threads")
-    it.stop_ev.set()
-    ct.stop_ev.set()
-    # Waiting for the threads to stop
-    logger.info("Waiting for threads to stop")
-    it.join()
-    ct.join(timeout=2)
-    if ct.is_alive():
-        logger.error("THREAD NOT STOPPED!!!")
-    logger.info("EXITING")
+
+    apt.stop_ev.set()
+
+    print("STOPPING THREAD")
+    apt.join()
